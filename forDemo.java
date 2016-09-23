4ef8f7ba1216f77ac443400a81f698bd5ebec2f0
Kaustubh Sakhalkar <sakhalkark@gmail.com>
2016-09-23 18:38:50 +0530

    Add files via upload

---
+++ forDemo.java
@@ -0,0 +1,7 @@
+class forDemo{
+	public static void main(String args[]){
+		int x;
+		for(x = 0; x < 10; x++)
+			System.out.println("This is x:" + x);
+	}
+}
---
+++ variable.java
@@ -0,0 +1,10 @@
+class variable{
+	public static void main(String args[]){
+		int num;
+
+		num = 100;
+		System.out.println("num = " + num);
+		num = num * 2;
+		System.out.print("The num x 2 = " + num);
+	}
+}
